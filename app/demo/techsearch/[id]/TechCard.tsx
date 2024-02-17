903facfe800929cb47985fe26d5738cfc7293799
Luke <luke.zd.carpenter@gmail.com>
2024-02-17 17:01:34 +0000

    added tech page

---
+++ app/demo/techsearch/[id]/TechCard.tsx
@@ -0,0 +1,37 @@
+﻿import { TechItem } from "@/app/demo/techsearch/tech"
+import { subtitle, title } from "@/components/primitives"
+import { Card, CardBody, CardFooter, CardHeader } from "@nextui-org/card"
+import { Chip } from "@nextui-org/chip"
+import { Divider } from "@nextui-org/divider";
+
+export default function TechCard({ techItem }: { techItem: TechItem }) {
+
+    return (
+        <Card>
+            <CardHeader className="flex gap-3">
+                <h1 className={title({ size: 'sm' })}>{techItem.displayName}</h1>
+            </CardHeader>
+            <Divider />
+            <CardBody className="flex-col gap-3 ">
+                <section>
+                    <h3 className={subtitle()}>Description</h3>
+                    <p>Description goes here...</p>
+                </section>
+                <section>
+                    <h3 className={subtitle()}>Tags</h3>
+                    <div className="flex gap-2">
+                        {techItem.tags.map((tag) => (
+                            <Chip key={tag}>{tag}</Chip>
+                        ))}
+                    </div>
+                </section>
+
+
+            </CardBody>
+            {/* <CardFooter>
+
+            </CardFooter> */}
+        </Card >
+
+    );
+}
---
+++ app/demo/techsearch/[id]/page.tsx
@@ -0,0 +1,27 @@
+﻿import TechCard from "@/app/demo/techsearch/[id]/TechCard";
+import { TechItem, getById, techItems } from "@/app/demo/techsearch/tech";
+import { title } from "@/components/primitives";
+
+export default function Page(props: { params: { id: string } }) {
+
+    const techItem = getById(props.params.id)
+    if (techItem == null) {
+        return (
+            <>
+                <p>Not Found</p>
+            </>
+        )
+    }
+
+    return (
+        <TechCard techItem={techItem} />
+    )
+
+}
+
+export async function generateStaticParams() {
+
+    return techItems.map((item) => ({
+        id: item.id,
+    }))
+}
